98d48836a1a9e4bc019559f75f351cc8950af45a
GHXX <GHXX@gmx.at>
2024-11-14 01:29:13 +0100

    add JLuaVM submodule

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import dev.asdf00.jluavm.internals.LuaVM_RT;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        var vm = LuaVM_RT.create();
     }
 }
