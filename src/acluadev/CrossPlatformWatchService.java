4d295ba26831293c72a0553b91733998ed22ac51
GHXX <GHXX@gmx.at>
2025-08-10 04:06:09 +0200

    also allow symlinks for filewatching

--- src/acluadev/CrossPlatformWatchService.java
+++ src/acluadev/CrossPlatformWatchService.java
@@ -11,7 +11,7 @@ public class CrossPlatformWatchService {
     public static void waitForChange(Path rootDir) throws InterruptedException {
         var fs = FileSystems.getDefault();
         try (WatchService ws = fs.newWatchService()) {
-            try (var w = Files.walk(rootDir)) {
+            try (var w = Files.walk(rootDir, FileVisitOption.FOLLOW_LINKS)) {
                 for (var dir : w.filter(Files::isDirectory).toArray(Path[]::new)) {
                     dir.register(ws, new WatchEvent.Kind[]{ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE}, SensitivityWatchEventModifier.HIGH);
                 }
