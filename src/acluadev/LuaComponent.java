779200a4e51588b7ffbd3bd59d36ba4446d51cd0
GHXX <GHXX@gmx.at>
2025-10-01 23:34:26 +0200

    cleanup

--- src/acluadev/LuaComponent.java
+++
@@ -1,9 +0,0 @@
-package acluadev;
-
-import dev.asdf00.jluavm.runtime.types.LuaObject;
-
-public record LuaComponent(String type, LuaObject comp) {
-    public LuaObject[] asLuaObj() {
-        return new LuaObject[]{LuaObject.of(type), comp};
-    }
-}
