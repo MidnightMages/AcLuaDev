ff3b1ccdba986d1a076708dd25fc330e70ffd8a8
GHXX <GHXX@gmx.at>
2025-10-02 21:40:03 +0200

    add vm.listReadableUDKeys and vm.listWritableUDKeys

--- lua/AdvancedOS/disk1/boot.lua
+++ lua/AdvancedOS/disk1/boot.lua
@@ -6,6 +6,14 @@ function _G.pp(tbl)
         print(k, ":", v)
     end
 end
+       
+local ud = bootDrive
+print("------------------READABLE------------------")
+pp(vm.listReadableUDKeys(ud))
+print("--------------------------------------------")
+print("------------------WRITABLE------------------")
+pp(vm.listWritableUDKeys(ud))
+print("--------------------------------------------")
 
 _ENV._EXT.string = _ENV.string
 function string.endsWith(str, suffix) return string.sub(str, #str-#suffix+1) == suffix end
