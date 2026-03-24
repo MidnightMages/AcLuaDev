31dce73295d380b31975edff68ebed64b0a35c54
00asdf <christoph@00asdf.dev>
2026-03-24 22:18:54 +0100

    print and printInline share screenpos

--- lua/AdvancedOS/uefi.lua
+++ lua/AdvancedOS/uefi.lua
@@ -19,7 +19,7 @@ _ENV = {
 ]]
 print("heartbeat")
 
-local SLEEP_TIME<const> = 3
+local SLEEP_TIME <const> = 3
 
 
 -- initialize printing
@@ -27,28 +27,28 @@ local gpu = components:getFirst("gpu")
 -- we assume each GPU to at least provide space for 110x44 characters
 local screenSizeX, screenSizeY = 110, 44
 local globalStdOutBuffer = gpu:newBuffer(screenSizeX, screenSizeY)
-for t,v in components:list() do
-    if t == "screen" then
-        gpu:assignBuffer(globalStdOutBuffer, v)
-    end
+for t, v in components:list() do
+  if t == "screen" then
+    gpu:assignBuffer(globalStdOutBuffer, v)
+  end
 end
 
+local cursorX, cursorY = 0, 0   -- to be captured by new prints
 local function makePrint(target, append)
-    local cursorX, cursorY = 0, 0 -- to be captured by new print
-    local function doPrint(...) -- new printing to screen
-        cursorX, cursorY = globalStdOutBuffer:pasteText(
-                cursorX, cursorY, "SCROLL_SPILL_CLEAR",
-                table.concat(table.pack(...), " ")..append)
+  local function doPrint(...)   -- new printing to screen
+    cursorX, cursorY = globalStdOutBuffer:pasteText(
+      cursorX, cursorY, "SCROLL_SPILL_CLEAR",
+      table.concat(table.pack(...), " ") .. append)
+  end
+  local oldPrint <const> = _ENV[target]
+  if oldPrint ~= nil then   -- possibly prepend console print
+    _ENV[target] = function(...)
+      oldPrint(...)
+      doPrint(...)
     end
-    local oldPrint<const> = _ENV[target]
-    if oldPrint ~= nil then -- possibly prepend console print
-        _ENV[target] = function(...)
-            oldPrint(...)
-            doPrint(...)
-        end
-    else
-        _ENV[target] = doPrint
-    end
+  else
+    _ENV[target] = doPrint
+  end
 end
 makePrint("print", "\n")
 makePrint("printInline", "")
@@ -61,11 +61,11 @@ local bootOptionsSleepTime
 local stackTraceOnCrash
 local computer = components:getFirst("computer")
 if computer.nvram ~= nil then
-    print("nvram is available")
-    bootOptionsSleepTime = computer.nvram.uefi_bootOptionsSleepTime
-    stackTraceOnCrash = computer.nvram.uefi_stackTraceOnCrash
+  print("nvram is available")
+  bootOptionsSleepTime = computer.nvram.uefi_bootOptionsSleepTime
+  stackTraceOnCrash = computer.nvram.uefi_stackTraceOnCrash
 else
-    print("nvram is unavailable")
+  print("nvram is unavailable")
 end
 bootOptionsSleepTime = bootOptionsSleepTime ~= nil and bootOptionsSleepTime or SLEEP_TIME
 stackTraceOnCrash = stackTraceOnCrash ~= nil and stackTraceOnCrash or false
@@ -73,53 +73,54 @@ stackTraceOnCrash = stackTraceOnCrash ~= nil and stackTraceOnCrash or false
 
 -- init boot function
 local function bootFromMedium(medium)
-    _ENV.bootDrive = medium -- set boot drive
-    local code = medium:open("boot.lua"):read()
-    print("loading boot.lua ...")
-    local bootFunc, errMsg = load(code, "boot.lua")
-    if not bootFunc then
-        print("ERROR loading boot.lua!\nexiting ...")
-        sleep(SLEEP_TIME)
-        return
+  _ENV.bootDrive = medium   -- set boot drive
+  local code = medium:open("boot.lua"):read()
+  print("loading boot.lua ...")
+  local bootFunc, errMsg = load(code, "boot.lua")
+  if not bootFunc then
+    print("ERROR loading boot.lua!\nexiting ...")
+    sleep(SLEEP_TIME)
+    return
+  end
+  -- save stuff we want to use after running the bootloader
+  print("initialize exit handling ...")
+  local saved <const> = {
+    setReboot = setReboot,
+    yield = coroutine.yield,
+    print = print,
+    pack = table.pack,
+    unpack = table.unpack,
+  }
+  setReboot = nil   -- we do not make that available to guest OS
+  function shutdown(isReboot)
+    saved.yield("shutdown", isReboot)
+  end
+
+  if stackTraceOnCrash then
+    bootFunc = function()
+      local result = saved.pack(xpcall(code, debug.traceback))
+      if result[1] then
+        return saved.unpack(result, 2, result.n)         -- spread results
+      else
+        error(result[2])                                 -- propagate error
+      end
     end
-    -- save stuff we want to use after running the bootloader
-    print("initialize exit handling ...")
-    local saved<const> = {
-            setReboot = setReboot,
-            yield = coroutine.yield,
-            print = print,
-            pack = table.pack,
-            unpack = table.unpack,
-    }
-    setReboot = nil -- we do not make that available to guest OS
-    function shutdown(isReboot)
-        saved.yield("shutdown", isReboot)
+  end
+  -- let's boot for real
+  print("booting ...")
+  local result = saved.pack(coroutine.resume(coroutine.create(bootFunc)))
+  if not result[1] then
+    -- some error has occurred, try printing error
+    local errMsg = tostring(result[2])
+    pcall(saved.print("UEFI: OS has crashed:\n" .. errMsg))
+    error("OS ERROR: " .. errMsg)
+    return
+  else
+    -- successful exit
+    if result[2] == "shutdown" then
+      saved.setReboot(result[3])
     end
-    if stackTraceOnCrash then
-        bootFunc = function ()
-            local result = saved.pack(xpcall(code, debug.traceback))
-            if result[1] then
-                return saved.unpack(result, 2, result.n) -- spread results
-            else
-                error(result[2]) -- propagate error
-            end
-        end
-    end
-    -- let's boot for real
-    print("booting ...")
-    local result = saved.pack(coroutine.resume(coroutine.create(bootFunc)))
-    if not result[1] then
-        -- some error has occurred, try printing error
-        local errMsg = tostring(result[2])
-        pcall(saved.print("UEFI: OS has crashed:\n"..errMsg))
-        error("OS ERROR: "..errMsg)
-        return
-    else
-        -- successful exit
-        if result[2] == "shutdown" then
-            saved.setReboot(result[3])
-        end
-    end
+  end
 end
 
 
@@ -129,60 +130,60 @@ local defaultBoot
 local idx = 1
 local bootables = {}
 for compType, elem in components:list() do
-    print("- "..compType)
-    if compType == "massStorage" then
-        if elem:fileExists("boot.lua") then
-            -- we found a bootable medium, use it as default if not already set
-            defaultBoot = defaultBoot == nil and idx or defaultBoot
-            bootables[idx] = elem
-        end
-        idx = idx + 1
+  print("- " .. compType)
+  if compType == "massStorage" then
+    if elem:fileExists("boot.lua") then
+      -- we found a bootable medium, use it as default if not already set
+      defaultBoot = defaultBoot == nil and idx or defaultBoot
+      bootables[idx] = elem
     end
+    idx = idx + 1
+  end
 end
 
 
 -- boot options
 if #bootables < 1 then
-    print("no bootable medium found!\nexiting ...")
-    sleep(SLEEP_TIME)
-    return
+  print("no bootable medium found!\nexiting ...")
+  sleep(SLEEP_TIME)
+  return
 elseif #bootables == 1 then
-    local bootTarget = bootables[defaultBoot]
-    print("booting from medium-"..defaultBoot.."-"..bootTarget.storageFamilyName.." ...")
-    sleep(bootOptionsSleepTime)
-    bootFromMedium(bootTarget)
+  local bootTarget = bootables[defaultBoot]
+  print("booting from medium-" .. defaultBoot .. "-" .. bootTarget.storageFamilyName .. " ...")
+  sleep(bootOptionsSleepTime)
+  bootFromMedium(bootTarget)
 else
-    print("\nboot options (default is top)")
-    for idx, medium in pairs(bootables) do
-        print("- medium-"..idx.."-"..medium.storageFamilyName)
-    end
-    local bootTarget = defaultBoot
-    local remaining = bootOptionsSleepTime * 10
+  print("\nboot options (default is top)")
+  for idx, medium in pairs(bootables) do
+    print("- medium-" .. idx .. "-" .. medium.storageFamilyName)
+  end
+  local bootTarget = defaultBoot
+  local remaining = bootOptionsSleepTime * 10
+  while true do
+    printInline("enter boot medium id: ")     -- only single char allowed
     while true do
-        printInline("enter boot medium id: ") -- only single char allowed
-        while true do
-            local nextEvent = computer:getMachineEvent()
-            if nextEvent[1] == "keyTyped" then
-                local requested = nextEvent[2]
-                print(requested)
-                if bootables[requested] ~= nil then
-                    bootTarget = bootables[requested]
-                    print("booting medium-"..requested.."-"..bootTarget.storageFamilyName)
-                    goto bootingLabel
-                else
-                    print("invalid medium id")
-                    remaining = -1 -- disable default timeout
-                    break
-                end
-            end
-            if remaining == 0 then -- ignore negatives
-                print("choosing default ...")
-                goto bootingLabel
-            end
-            remaining = remaining - 1
-            sleep(0.1)
+      local nextEvent = computer:getMachineEvent()
+      if nextEvent[1] == "keyTyped" then
+        local requested = nextEvent[2]
+        print(requested)
+        if bootables[requested] ~= nil then
+          bootTarget = bootables[requested]
+          print("booting medium-" .. requested .. "-" .. bootTarget.storageFamilyName)
+          goto bootingLabel
+        else
+          print("invalid medium id")
+          remaining = -1           -- disable default timeout
+          break
         end
+      end
+      if remaining == 0 then       -- ignore negatives
+        print("choosing default ...")
+        goto bootingLabel
+      end
+      remaining = remaining - 1
+      sleep(0.1)
     end
-    ::bootingLabel::
-    bootFromMedium(bootTarget)
+  end
+  ::bootingLabel::
+  bootFromMedium(bootTarget)
 end
