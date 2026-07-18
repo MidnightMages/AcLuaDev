f341c0b3d5618ba1a87a2e67fbf35441b2fb0fce
GHXX <GHXX@gmx.at>
2026-07-18 03:17:09 +0200

    add tiny lua uefi

--- lua/AdvancedOS/uefi.lua
+++ lua/AdvancedOS/uefi.lua
@@ -1,192 +1,66 @@
---[[
-Default UEFI implementation of AdvancedComputers
-This is the entry point of execution
+local c = components
+local var1 = c:getFirst("gpu")
+local buf = var1:newBuffer(110, 44)
+var1:assignBuffer(buf,c:getFirst("screen"))
+var1 = c:getFirst("computer")
 
-If NV-RAM is available, we try accessing the following options with their respective defaults:
-- uefi_bootOptionsSleepTime = 3 [sleep time before taking default boot option in seconds]
-- uefi_stackTraceOnCrash = false [if a stack trace is printed upon crash of OS]
-- uefi_bootDrive = "" [path to drive to boot from] !!! NOT IMPLEMENTED YET !!!
-
-Assumed _ENV on boot:
-_ENV = {
-    ... builtins ...,
-    components,
-    print
-    printInline
-    sleep
-    setReboot
-}
-]]
-print("heartbeat")
-
-local SLEEP_TIME <const> = 3
-
-
--- initialize printing
-local gpu = components:getFirst("gpu")
--- we assume each GPU to at least provide space for 110x44 characters
-local screenSizeX, screenSizeY = 110, 44
-local globalStdOutBuffer = gpu:newBuffer(screenSizeX, screenSizeY)
-for t, v in components:list() do
-    if t == "screen" then
-        gpu:assignBuffer(globalStdOutBuffer, v)
-    end
+local cursorX, cursorY = 0, 0
+local t = table
+function write(...)
+    cursorX, cursorY = buf:pasteText(cursorX, cursorY, "SCROLL_SPILL_CLEAR", t.concat(t.pack(...), " "))
 end
+function print(...)
+    write(t.concat(t.pack(...)," ").."\n")
+end
+local w = write
+local p = print
+p("Lua shell:")
 
-local cursorX, cursorY = 0, 0 -- to be captured by new prints
-local function makePrint(target, append)
-    local function doPrint(...) -- new printing to screen
-        cursorX, cursorY = globalStdOutBuffer:pasteText(
-            cursorX, cursorY, "SCROLL_SPILL_CLEAR",
-            table.concat(table.pack(...), " ") .. append)
-    end
-    local oldPrint <const> = _ENV[target]
-    if oldPrint ~= nil then -- possibly prepend console print
-        _ENV[target] = function(...)
-            oldPrint(...)
-            doPrint(...)
+w(">>> ")
+local line = ""
+local caretShown = false
+local untilCaretFlip = 0
+while""do
+    local e1,stringRepr=var1:getMachineEvent()
+    if not e1 then
+        sleep(0.05)
+        untilCaretFlip = untilCaretFlip - 0.05
+        if untilCaretFlip < 0 then
+            w(caretShown and "\b" or "_")
+            caretShown = not caretShown
+            untilCaretFlip = 0.5
         end
-    else
-        _ENV[target] = doPrint
-    end
-end
-makePrint("print", "\n")
-makePrint("printInline", "")
-print("booting UEFI ...\nprinting initialized")
-_ENV.uefiTextBuffer = globalStdOutBuffer -- inform OS about our text buffer
-
-
--- check NV-RAM and possibly load config
-local bootOptionsSleepTime
-local stackTraceOnCrash
-local computer = components:getFirst("computer")
-if computer.nvram ~= nil then
-    print("nvram is available")
-    bootOptionsSleepTime = computer.nvram.uefi_bootOptionsSleepTime
-    stackTraceOnCrash = computer.nvram.uefi_stackTraceOnCrash
-else
-    print("nvram is unavailable")
-end
-bootOptionsSleepTime = bootOptionsSleepTime or SLEEP_TIME
-stackTraceOnCrash = not not stackTraceOnCrash
-
-
--- init boot function
-local function bootFromMedium(medium)
-    _ENV.bootDrive = medium -- set boot drive
-    local bootHandle = medium:open("boot.lua")
-    local code = bootHandle:read(-1)
-    bootHandle:close()
-    print("loading boot.lua ...")
-    local bootFunc, errMsg = load(code, "boot.lua")
-    if not bootFunc then
-        print("ERROR loading boot.lua!\nexiting ...")
-        sleep(SLEEP_TIME)
-        return
-    end
-    -- save stuff we want to use after running the bootloader
-    print("initialize exit handling ...")
-    local saved <const> = {
-        setReboot = setReboot,
-        yield = coroutine.yield,
-        print = print,
-        pack = table.pack,
-        unpack = table.unpack,
-    }
-    setReboot = nil -- we do not make that available to guest OS
-    function shutdown(isReboot)
-        saved.yield("shutdown", isReboot)
-    end
-
-    if stackTraceOnCrash then
-        local realBootFunc = bootFunc
-        bootFunc = function()
-            local result = saved.pack(xpcall(realBootFunc, debug.traceback))
-            if result[1] then
-                return saved.unpack(result, 2, result.n) -- spread results
-            else
-                error(result[2], 0)                 -- propagate error
+    elseif e1 == "shutdown" then
+        break
+    elseif e1 == "keyPressed" then
+        if stringRepr ~= "" then
+            if caretShown then
+                caretShown = false
+                w("\b")
+                untilCaretFlip = 0
             end
-        end
-    end
-    -- let's boot for real
-    print("booting ...")
-    local result = saved.pack(coroutine.resume(coroutine.create(bootFunc)))
-    print ("post echo")
-    if not result[1] then
-        -- some error has occurred, try printing error
-        local errMsg = tostring(result[2])
-        pcall(saved.print, "UEFI: OS has crashed:\n" .. errMsg)
-        error("OS ERROR: " .. errMsg)
-        return
-    else
-        -- successful exit
-        if result[2] == "shutdown" then
-            saved.setReboot(result[3])
-        end
-    end
-end
-
-
--- find bootable media
-print("\navailable components:")
-local defaultBoot
-local idx = 1
-local bootables = {}
-for compType, elem in components:list() do
-    print("- " .. compType)
-    if compType == "massStorage" then
-        if elem:fileExists("boot.lua") then
-            -- we found a bootable medium, use it as default if not already set
-            defaultBoot = defaultBoot == nil and idx or defaultBoot
-            bootables[idx] = elem
-        end
-        idx = idx + 1
-    end
-end
-
-
--- boot options
-if #bootables < 1 then
-    print("no bootable medium found!\nexiting ...")
-    sleep(SLEEP_TIME)
-    return
-elseif #bootables == 1 then
-    local bootTarget = bootables[defaultBoot]
-    print("booting from medium-" .. defaultBoot .. "-" .. bootTarget.storageFamilyName .. " ...")
-    bootFromMedium(bootTarget)
-else
-    print("\nboot options (default is top)")
-    for idx, medium in pairs(bootables) do
-        print("- medium-" .. idx .. "-" .. medium.storageFamilyName)
-    end
-    local bootTarget = defaultBoot
-    local remaining = bootOptionsSleepTime * 10
-    while true do
-        printInline("enter boot medium id: ") -- only single char allowed
-        while true do
-            local nextEvent = computer:getMachineEvent()
-            if nextEvent[1] == "keyTyped" then
-                local requested = nextEvent[2]
-                print(requested)
-                if bootables[requested] ~= nil then
-                    bootTarget = bootables[requested]
-                    print("booting medium-" .. requested .. "-" .. bootTarget.storageFamilyName)
-                    goto bootingLabel
-                else
-                    print("invalid medium id")
-                    remaining = -1 -- disable default timeout
-                    break
+            if stringRepr == "\b" then
+                if #line > 0 then
+                    w("\b")
+                    line = line:sub(1,-2)
                 end
+            elseif stringRepr == "\n" then
+                w("\n")
+                local f, err = load(line)
+                if not f then
+                    p("Error: "..err)
+                else
+                    local ok, res = xpcall(f, debug.traceback)
+                    if not ok then
+                        p("Error: ",tostring(res))
+                    end
+                end
+                w(">>> ")
+                line=""
+            else
+                w(stringRepr)
+                line = line .. stringRepr
             end
-            if remaining == 0 then -- ignore negatives
-                print("choosing default ...")
-                goto bootingLabel
-            end
-            remaining = remaining - 1
-            sleep(0.1)
         end
     end
-    ::bootingLabel::
-    bootFromMedium(bootTarget)
 end
